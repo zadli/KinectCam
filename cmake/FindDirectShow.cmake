2379fff64bcff8ade043785d7bfe5994ba2a2f97
Joseph Davies <deledrius@gmail.com>
2019-08-16 03:51:49 -0700

    Remove detection for unused DShow component.

--- cmake/FindDirectShow.cmake
+++ cmake/FindDirectShow.cmake
@@ -71,82 +71,6 @@ find_path(DIRECTSHOW_WINDOWSSDK_INCLUDE_DIR
 	PATH_SUFFIXES
 	"Include")
 
-###
-# Begin code dedicated to finding a proper qedit.h file...
-###
-
-# Checks to see if a directory has qedit.h and if that file contains ISampleGrabber.
-function(_directshow_check_qedit _dir _var)
-	set(fn "${_dir}/qedit.h")
-	if(NOT EXISTS "${fn}")
-		set(${_var} FALSE PARENT_SCOPE)
-		return()
-	endif()
-	file(STRINGS "${fn}" samplegrabber REGEX ISampleGrabber)
-	if(NOT samplegrabber)
-		set(${_var} FALSE PARENT_SCOPE)
-		return()
-	endif()
-	set(${_var} TRUE PARENT_SCOPE)
-endfunction()
-
-function(_directshow_check_current_qedit)
-	if(DIRECTSHOW_QEDIT_INCLUDE_DIR)
-		_directshow_check_qedit("${DIRECTSHOW_QEDIT_INCLUDE_DIR}" _ds_ok)
-		if(NOT _ds_ok)
-			message(STATUS "FindDirectShow: qedit.h in ${DIRECTSHOW_QEDIT_INCLUDE_DIR} lacks ISampleGrabber, unsetting DIRECTSHOW_QEDIT_INCLUDE_DIR")
-			set(DIRECTSHOW_QEDIT_INCLUDE_DIR "" CACHE PATH "" FORCE)
-		endif()
-	endif()
-endfunction()
-
-# Check before deciding if we should make our list of possible locations.
-_directshow_check_current_qedit()
-
-# Compose a list of possible directories that might hold a qedit.h file.
-set(DIRECTSHOW_QEDIT_SEARCH)
-if(WINDOWSSDK_FOUND AND NOT DIRECTSHOW_QEDIT_INCLUDE_DIR)
-	foreach(_sdk ${WINDOWSSDK_DIRS})
-		windowssdk_build_lookup("${_sdk}" _build)
-		if(_build AND ("${_build}" VERSION_LESS 6.2))
-		get_windowssdk_include_dirs("${_sdk}" _dirs)
-		if(_dirs)
-			list(APPEND DIRECTSHOW_QEDIT_SEARCH ${_dirs})
-		endif()
-		endif()
-	endforeach()
-endif()
-
-# This one we can grab from another SDK version.
-find_path(DIRECTSHOW_QEDIT_INCLUDE_DIR
-	NAMES
-	qedit.h
-	HINTS
-	"${DIRECTSHOW_WINDOWSSDK_ROOT}"
-	PATHS
-	${DIRECTSHOW_QEDIT_SEARCH}
-	PATH_SUFFIXES
-	"Include")
-
-# Check if the directory is OK after the search.
-_directshow_check_current_qedit()
-
-# If we didn't find a proper qedit, manually look through the possibilities.
-if(NOT DIRECTSHOW_QEDIT_INCLUDE_DIR)
-	foreach(_dir "${DIRECTSHOW_WINDOWSSDK_ROOT}/Include" ${DIRECTSHOW_QEDIT_SEARCH})
-		if(NOT DIRECTSHOW_QEDIT_INCLUDE_DIR)
-			_directshow_check_qedit("${_dir}" _ds_ok)
-			if(_ds_ok)
-				set(DIRECTSHOW_QEDIT_INCLUDE_DIR "${_dir}" CACHE PATH "" FORCE)
-			endif()
-		endif()
-	endforeach()
-endif()
-
-###
-# End qedit.h section.
-###
-
 set(DIRECTSHOW_STRMIIDS_SEARCH)
 if(WINDOWSSDK_FOUND AND NOT DIRECTSHOW_STRMIIDS_LIBRARY)
 	foreach(_sdk ${WINDOWSSDK_DIRS})
@@ -172,7 +96,6 @@ find_package_handle_standard_args(DirectShow
 	DEFAULT_MSG
 	DIRECTSHOW_BASECLASS_DIR
 	DIRECTSHOW_WINDOWSSDK_INCLUDE_DIR
-	DIRECTSHOW_QEDIT_INCLUDE_DIR
 	DIRECTX_INCLUDE_DIR
 	DIRECTSHOW_STRMIIDS_LIBRARY)
 
@@ -188,10 +111,6 @@ if(DIRECTSHOW_FOUND)
 			DIRECTSHOW_INCLUDE_DIRS
 			"${DIRECTSHOW_WINDOWSSDK_INCLUDE_DIR}/atl")
 	endif()
-	if(NOT "${DIRECTSHOW_QEDIT_INCLUDE_DIR}" STREQUAL "${DIRECTSHOW_WINDOWSSDK_INCLUDE_DIR}")
-		# QEdit include dir might be an older SDK, so put it last.
-		list(APPEND DIRECTSHOW_INCLUDE_DIRS "${DIRECTSHOW_QEDIT_INCLUDE_DIR}")
-	endif()
 
 	set(DIRECTSHOW_LIBRARIES "${DIRECTSHOW_STRMIIDS_LIBRARY}")
 
@@ -200,5 +119,4 @@ endif()
 
 mark_as_advanced(DIRECTSHOW_BASECLASS_DIR
 	DIRECTSHOW_WINDOWSSDK_INCLUDE_DIR
-	DIRECTSHOW_QEDIT_INCLUDE_DIR
 	DIRECTSHOW_STRMIIDS_LIBRARY)
